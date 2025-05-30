14e15da30da5a69ca20b100856e9ed6e368ddee9
desainiravp <125334778+desainiravp@users.noreply.github.com>
2025-05-30 15:45:13 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "eks_sg" {
+  name        = "eks_sg"
+  description = "Allow access to the EKS cluster"
+  vpc_id      = aws_vpc.main.id
+}
